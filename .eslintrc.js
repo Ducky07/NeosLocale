6fca4de09e7cad368aec8b10b211e5f3f53e1e81
Jack Valley <juuso.laakso@turku.fi>
2021-07-01 15:33:36 +0300

    Added Github Actions
    Made a workflow to validate locale the locale files and check for errors.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'extends': ['plugin:jsonc/recommended-with-jsonc'],
+  'rules': {
+    'no-tabs': 'error',
+    'no-empty': 'error',
+    'no-trailing-spaces': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+  },
+};
